0d529e35f7acfe55e47a0679a468cc725a216c89
Md Sayem Hossain <sayemawasome@gmail.com>
2023-11-28 20:41:44 +0800

    a

--- src/app/core/js/test.js
+++ src/app/core/js/test.js
@@ -223,4 +223,12 @@ function displayCounter() {
 }
 window.onload = function () {
  displayCounter();
+
+ 
+   window.dataLayer = window.dataLayer || [];
+   function gtag(){dataLayer.push(arguments);}
+   gtag('js', new Date());
+ 
+   gtag('config', 'G-FP6H7C6JK7'); 
+
 }
