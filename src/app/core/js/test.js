30b026598e39f14baeecf5b00123c18cdacaa1b4
Md Sayem Hossain <sayemawasome@gmail.com>
2023-11-29 01:31:22 +0800

    adding new gts

--- src/app/core/js/test.js
+++ src/app/core/js/test.js
@@ -224,11 +224,6 @@ function displayCounter() {
 window.onload = function () {
  displayCounter();
 
- 
-   window.dataLayer = window.dataLayer || [];
-   function gtag(){dataLayer.push(arguments);}
-   gtag('js', new Date());
- 
-   gtag('config', 'G-FP6H7C6JK7'); 
+  
 
 }
